f6c1dadb6dd327db822db75f2596f97ed95a40ce
VasyaRki <95859396+VasyaRki@users.noreply.github.com>
2022-10-06 14:53:15 +0300

    Update PrintStats.cpp

--- PrintStats.cpp
+++ PrintStats.cpp
@@ -54,16 +54,16 @@ void PrintStats(vector<Person> persons)
 }
 
 int main() {
-    // vector<Person> persons = {
-    //     {31, Gender::MALE, false},
-    //     {40, Gender::FEMALE, true},
-    //     {24, Gender::MALE, true},
-    //     {20, Gender::FEMALE, true},
-    //     {80, Gender::FEMALE, false},
-    //     {78, Gender::MALE, false},
-    //     {10, Gender::FEMALE, false},
-    //     {55, Gender::MALE, true},
-    // };
-  //PrintStats(persons);
+  vector<Person> persons = {
+         {31, Gender::MALE, false},
+         {40, Gender::FEMALE, true},
+         {24, Gender::MALE, true},
+         {20, Gender::FEMALE, true},
+         {80, Gender::FEMALE, false},
+         {78, Gender::MALE, false},
+         {10, Gender::FEMALE, false},
+         {55, Gender::MALE, true},
+     };
+  PrintStats(persons);
   return 0;
 }
